15027fcd7826e52a34cbb03c74c93f1a8bbda0d7
Neptunium6195 <seratesarah@gmail.com>
2025-12-18 09:46:21 -0800

    portal + books
    Finished portal mechanics, added dialogue to the bookshelf

--- portal.gd
+++ portal.gd
@@ -1,18 +1,10 @@
 extends Node2D
-var placedCorrectly = 0
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Dialogic.timeline_ended.connect(_on_dialog_finished)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if placedCorrectly == 3:
-		start_dialog()
-
-func start_dialog() -> void:
-	Dialogic.start("res://Timelines/bookPuzzle.dtl")
-
-func _on_dialog_finished(_timeline_name: String) -> void:
 	pass

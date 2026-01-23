ec099552b0c252ac95b7ac7655ee6336b7806c12
ctln-d <caitlindu10@gmail.com>
2026-01-22 21:28:26 -0500

    basic mechanics

--- dial.gd
+++ dial.gd
@@ -2,49 +2,26 @@ extends Area2D
 
 @export var current_value := 0
 
-var dragging := false
-var drag_start_y := 0.0
-var accumulated_drag := 0.0
-const PIXELS_PER_STEP := 64  # height of one digit in pixels
+var base_sprite_pos: Vector2
 
 func _ready():
+	var sprite = get_parent().get_node("Sprite2D")
+	base_sprite_pos = sprite.position
 	update_visual()
 
+
+# when clicked the vertical numbers will scroll up (0-1, 1-2, 2-3, etc)
 func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed:
-			dragging = true
-			drag_start_y = event.position.y  # local to Area2D
-			accumulated_drag = 0.0
-		else:
-			dragging = false
-
-func _process(_delta):
-	if not dragging:
-		return
-
-	var mouse_y = get_local_mouse_position().y
-	var delta = mouse_y - drag_start_y
-	drag_start_y = mouse_y
-	accumulated_drag += delta
-
-	while abs(accumulated_drag) >= PIXELS_PER_STEP:
-		if accumulated_drag > 0:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			increment()
-			accumulated_drag -= PIXELS_PER_STEP
-		else:
-			decrement()
-			accumulated_drag += PIXELS_PER_STEP
 
 func increment():
-	current_value = (current_value + 1) % 10
+	current_value = (current_value + 1) % 10    # modulus wraps around to 0
 	update_visual()
 
-func decrement():
-	current_value = (current_value - 1 + 10) % 10
-	update_visual()
-
+# move to next number
 func update_visual():
 	var sprite = get_parent().get_node("Sprite2D")
 	var digit_height = sprite.texture.get_height() / 10.0
-	sprite.position = Vector2(0, -current_value * digit_height)
+	sprite.position = base_sprite_pos + Vector2(0, -current_value * digit_height)
